aaebc341db4a986aed05960cb3827302f06809a5
Andrey <duzhak.a55@gmail.com>
2020-12-07 17:54:00 +0200

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Привiт, ДонНУ");
+        }
+            
+    }
+}
